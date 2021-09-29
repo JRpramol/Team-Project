3207a20a5e40f6159b8ca4504f3961e69e9967e6
Gabriel Prieto <gabo.prieto@yahoo.com>
2021-09-28 23:37:02 -0400

    changed api

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/", function(req, res) {
 });
 // serving data
 app.get('/api/', (req, res) => {
-    res.json({group15: "software engineering"})
+    res.json({group15: "software engineering - new change"})
 })
 
 app.listen(PORT, function() {
