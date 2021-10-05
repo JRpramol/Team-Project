98b111a2a7e74afba7e103f446be8ce2b8834205
Gabriel Prieto <gabo.prieto@yahoo.com>
2021-10-04 23:06:30 -0400

    exported db object

--- db-connection.js
+++ db-connection.js
@@ -1,12 +1,13 @@
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "us-cdbr-east-04.cleardb.com",
-    user: "b8392740cb5edc",
-    password: "20ad70dc"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+const mysql = require('mysql');
+
+// Create a connection to the database
+const db = mysql.createPool({
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'b8392740cb5edc',
+    password: '20ad70dc',
+    database: 'heroku_30466051e354b84',
+    charset: 'utf8mb4'
+});
+
+module.exports = db;
